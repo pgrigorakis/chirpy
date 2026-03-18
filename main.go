251d12d07cbcab4c8cae8c54a2f668419a4eb78e
Panagiotis Grigorakis <p.grigorakis@gmail.com>
2026-03-18 22:23:42 +0100

    added endpoint for getting chirps

--- main.go
+++ main.go
@@ -52,6 +52,7 @@ func main() {
 	mux.HandleFunc("GET /admin/metrics", apiCfg.handlerMetrics)
 	mux.HandleFunc("POST /admin/reset", apiCfg.handlerReset)
 	mux.HandleFunc("POST /api/chirps", apiCfg.handlerChirpsCreate)
+	mux.HandleFunc("GET /api/chirps", apiCfg.handlerChirpsGet)
 	mux.HandleFunc("POST /api/users", apiCfg.handlerUsersCreate)
 
 	server := &http.Server{
