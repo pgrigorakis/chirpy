d224d894f5dcf718d98317a363b3634d1f1888f4
Panagiotis Grigorakis <p.grigorakis@gmail.com>
2026-03-18 12:04:37 +0100

    added database integration

--- main.go
+++ main.go
@@ -1,20 +1,41 @@
 package main
 
 import (
+	"database/sql"
 	"log"
 	"net/http"
+	"os"
 	"sync/atomic"
+
+	"github.com/joho/godotenv"
+	_ "github.com/lib/pq"
+	"github.com/pgrigorakis/chirpy/internal/database"
 )
 
 type apiConfig struct {
 	fileServerHits atomic.Int32
+	db             *database.Queries
 }
 
 func main() {
 	const filepathRoot = "."
 	const port = "8080"
 
-	apiCfg := apiConfig{}
+	godotenv.Load()
+	dbUrl := os.Getenv("DB_URL")
+	if dbUrl == "" {
+		log.Fatal("DB_URL must be set")
+	}
+	dbConn, err := sql.Open("postgres", dbUrl)
+	if err != nil {
+		log.Fatal("could not open database: %w", err)
+	}
+
+	dbQueries := database.New(dbConn)
+
+	apiCfg := &apiConfig{
+		fileServerHits: atomic.Int32{},
+		db:             dbQueries}
 
 	mux := http.NewServeMux()
 
