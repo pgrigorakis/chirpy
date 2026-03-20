c140e480d537022b2f1ec34ed98c3bbaddae749d
Panagiotis Grigorakis <p.grigorakis@gmail.com>
2026-03-20 23:43:56 +0100

    typo

--- internal/database/refresh_tokens.sql.go
+++ internal/database/refresh_tokens.sql.go
@@ -81,9 +81,9 @@ func (q *Queries) GetUserFromRefreshToken(ctx context.Context, token string) (uu
 const revokeToken = `-- name: RevokeToken :exec
 UPDATE refresh_tokens
 SET
-updated_at=$1,
-revoked_at=$2
-WHERE token=$3
+updated_at = $1,
+revoked_at = $2
+WHERE token = $3
 `
 
 type RevokeTokenParams struct {
